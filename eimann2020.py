ca49aad29bd1939164e56737941c1beba7e14e32
Chrias75 <christian_brueckner23@yahoo.de>
2020-11-04 15:20:37 +0100

    reworked configuration reading and mass fraction input into separat files

--- eimann2020.py
+++ eimann2020.py
@@ -1,78 +1,23 @@
 import numpy as np
-import configparser
+import read_config
 import fluid_properties_air as fpa
 import fluid_properties_water as fpw
 from CoolProp.CoolProp import PropsSI
 from CoolProp.HumidAirProp import HAPropsSI
 from scipy.integrate import quad
+import mass_fractions as mf
 
-re, pr, sc, t_in, t_out, t_w, t_mean, t_dp_in, t_dp_out, rH, m_air, m_water, m_cond = [], [], [], [], [], [], [], [], [], [], [], [], []
-b, h, l, p_standard, theta_a = 0., 0., 0., 0., 0.
+# re, pr, sc, t_in, t_out, t_w, t_mean, t_dp_in, t_dp_out, rH, m_air, m_water, m_cond = [], [], [], [], [], [], [], [], [], [], [], [], []
+# b, h, l, p_standard, theta_a = 0., 0., 0., 0., 0.
 ####################################################################################
 # Input
 ####################################################################################
-# switch = 'config'
-switch = 'dat'
+
+config_file = 'model_config.cfg'
 data = '/home/brue_ch/Auswertungen/rH_variable/Profil/data_rH_2000_27_5.dat'
 result_filename = '/home/brue_ch/Auswertungen/rH_variable/Profil/eimann_r-mod_2000_27_5.dat'
-if switch == 'config':
-    config = configparser.ConfigParser()
-    config.read('model_config.cfg')
-    cfg_numbers = config['dimless_numbers']
-    re = np.array([cfg_numbers.getfloat('reynolds')])
-    pr = np.array([cfg_numbers.getfloat('prandtl')])
-    sc = np.array([cfg_numbers.getfloat('schmidt')])
-
-    cfg_temp = config['temperatures']
-    t_in = np.array([cfg_temp.getfloat('t_in')])
-    t_out = np.array([cfg_temp.getfloat('t_out')])
-    t_w = np.array([cfg_temp.getfloat('t_w')])
-    t_mean = np.array([cfg_temp.getfloat('t_mean')])
-    t_dp_in = np.array([cfg_temp.getfloat('t_dp_in')])
-    t_dp_out = np.array([cfg_temp.getfloat('t_dp_out')])
-    rH = np.array([cfg_temp.getfloat('rH')])
-
-    cfg_dim = config['dimensions']
-    b = cfg_dim.getfloat('width')
-    h = cfg_dim.getfloat('height')
-    l = cfg_dim.getfloat('length')
-
-    cfg_oth = config['other']
-    m_air = np.array([cfg_oth.getfloat('massflow_moistair')])
-    m_water = np.array([cfg_oth.getfloat('massflow_water')])
-    m_cond = np.array([cfg_oth.getfloat('massflow_condensate')])
-    p_standard = cfg_oth.getfloat('pressure')
-    theta_a = cfg_oth.getfloat('ascending_contact_angle') / (180 * np.pi)
-elif switch == 'dat':
-    re = np.loadtxt(data, skiprows=1, usecols=2)
-    pr = np.loadtxt(data, skiprows=1, usecols=4)
-
-    t_in = np.loadtxt(data, skiprows=1, usecols=28)
-    t_out = np.loadtxt(data, skiprows=1, usecols=30)
-    t_w = np.loadtxt(data, skiprows=1, usecols=26)
-    t_mean = np.loadtxt(data, skiprows=1, usecols=32)
-    t_dp_in = np.loadtxt(data, skiprows=1, usecols=34)
-    t_dp_out = np.loadtxt(data, skiprows=1, usecols=36)
-    rH = np.loadtxt(data, skiprows=1, usecols=45)
-
-    m_air = np.loadtxt(data, skiprows=1, usecols=50)
-    m_water = np.loadtxt(data, skiprows=1, usecols=40)
-    m_cond = np.loadtxt(data, skiprows=1, usecols=18)
-
-    config = configparser.ConfigParser()
-    config.read('model_config.cfg')
-    cfg_dim = config['dimensions']
-    b = cfg_dim.getfloat('width')
-    h = cfg_dim.getfloat('height')
-    l = cfg_dim.getfloat('length')
-    p_standard = config['other'].getfloat('pressure')
-    theta_a = config['other'].getfloat('ascending_contact_angle') / (180 * np.pi)
-    sc = (fpa.moist_air_dynamic_viscosity(t_mean, p_standard, rH * fpa.temperature2saturation_vapour_pressure(t_in))) / \
-         (fpa.diffusion_coefficient(t_mean) *
-          fpa.moist_air_density(p_standard, rH * fpa.temperature2saturation_vapour_pressure(t_in), t_mean))
-else:
-    print('switch not set')
-    exit(0)
+re, pr, sc, t_in, t_out, t_w, t_mean, t_dp_in, t_dp_out, \
+ rH, mf_int, mf_bulk, b, h, l, p_standard, theta_a = read_config.read(config_file, result_filename, switch='config')
 ####################################################################################
 # Functions
 ####################################################################################
@@ -105,23 +50,6 @@ def Nu_sen(reynolds, prandtl, dia_hydr, length):
     else:
         gamma = (reynolds - 2300) / (10000 - 2300)
         return (1 - gamma) * Nu_lam(2300, prandtl, dia_hydr, length) + gamma * Nu_turb(10000, prandtl, dia_hydr, length)
-
-
-def mass_fraction_interface(pressure, temp_interface):
-    """noncondensable gas mass fraction at the gas-liquid interface with the assumption of saturation following the
-       Gibbs-Dalton Law as formulated by
-       G. Caruso, D. Di Vitale Maio, Heat and mass transfer analogy applied to condensation in the presence of
-       noncondensable gases inside inclined tubes, Int. J. Heat Mass Transf. 68 (2014) 401–414,
-       doi: 10.1016/j.ijheatmasstransfer.2013.09.049"""
-    p_sat = fpa.temperature2saturation_vapour_pressure(temp_interface)
-    M_v = fpa.MOLES_MASS_VAPOUR
-    M_air = fpa.MOLES_MASS_AIR
-    return (pressure - p_sat) / (pressure - (1 - M_v / M_air) * p_sat)
-
-
-def mass_fraction_bulk(mass_flow_ncg, mass_flow_water):
-    """noncondensable gas mass fraction in the bulk flow"""
-    return mass_flow_ncg / (mass_flow_water + mass_flow_ncg)
 
 
 def Nu_lat(sherwood, prandtl, schmidt, jakob, B_i):
@@ -190,9 +118,9 @@ def f_surf_tens(r_d, gamma, theta_max, theta_min):
     return r_d * gamma * quad(cos_theta, 0., 2. * np.pi, args=(theta_max, theta_min,))[0]
 
 
-def f_drag(r_d, density, u, C_d, theta_max, theta_min, beta):
+def f_drag(r_d, density, v, c_drag, theta_max, theta_min, beta):
     l_f = np.sin(theta_max) * (1 - np.cos(theta_min)) / (np.sin(theta_min) * (1 - np.cos(theta_max)))
-    return r_d ** 2 * 0.5 * density * u ** 2 * C_d / (1 + l_f) ** 2 * \
+    return r_d ** 2 * 0.5 * density * v ** 2 * c_drag / (1 + l_f) ** 2 * \
         (l_f ** 2 * theta_min / np.sin(beta) ** 2 + l_f ** 2 / np.tan(beta) + theta_min / np.sin(theta_min) ** 2 -
          1 / np.tan(theta_min))
 
@@ -225,11 +153,11 @@ rho_b = fpa.moist_air_density(p_standard, rH * fpa.temperature2saturation_vapour
 
 # surface tension force
 theta_r = 40.65 / (180 * np.pi)
-gamma = PropsSI('SURFACE_TENSION', 'T', t_mean + 273.15, 'Q', 1, 'Water')
+surf_tens = np.array([PropsSI('SURFACE_TENSION', 'T', t_mean + 273.15, 'Q', 1, 'Water')])
 
 r_max = np.full(re.shape, 0.0015)
-bo = rho_c * g * (2 * r_max) ** 2 / gamma
-# print(bo)
+bo = rho_c * g * (2 * r_max) ** 2 / surf_tens
+
 beta = 1 + 0.096 * bo
 theta_m = theta_a * (0.01 * bo ** 2 - 0.155 * bo + 0.97)
 re_d = re * r_max / d_h
@@ -237,6 +165,7 @@ c_d = 0.28 + (6 / np.sqrt(re_d)) + (21 / re_d)
 
 u = re * HAPropsSI('mu', 'T', t_mean + 273.15, 'P', p_standard, 'R', rH) / \
     (d_h * fpa.moist_air_density(p_standard, rH * fpa.temperature2saturation_vapour_pressure(t_in), t_mean))
+print('u: ', u)
 f_g = np.zeros(re.shape)
 f_s = np.zeros(re.shape)
 f_d = np.zeros(re.shape)
@@ -245,18 +174,18 @@ epsilon_1 = np.ones(re.shape)
 for i, item in enumerate(re):
     # print(i)
     # print('Re: ', item)
-    while abs(epsilon_1[i]) > 0.0000001:
-        bo[i] = rho_c[i] * g * (2 * r_max[i]) ** 2 / gamma[i]
+    while abs(epsilon_1[i]) > 1e-9:
+        bo[i] = rho_c[i] * g * (2 * r_max[i]) ** 2 / surf_tens[i]
         # print(bo)
         beta[i] = 1 + 0.096 * bo[i]
         theta_m = theta_a * (0.01 * bo[i] ** 2 - 0.155 * bo[i] + 0.97)
         re_d[i] = re[i] * r_max[i] / d_h
         c_d[i] = 0.28 + (6 / np.sqrt(re_d[i])) + (21 / re_d[i])
         f_g[i] = f_grav(r_max[i], rho_c[i])
-        f_s[i] = f_surf_tens(r_max[i], gamma[i], theta_a, theta_m)
+        f_s[i] = f_surf_tens(r_max[i], surf_tens[i], theta_a, theta_m)
         # print(f_s[i])
         f_d[i] = f_drag(r_max[i], rho_b[i], u[i], c_d[i], theta_a, theta_m, beta[i])
-        epsilon_1[i] = f_g[i] + f_d[i] + f_s[i]
+        epsilon_1[i] = f_g[i] ** 2 + f_d[i] ** 2 + f_s[i] ** 2
         # print('eps: ', epsilon_1)
         if epsilon_1[i] < 0.:
             r_max[i] += 0.00001
@@ -267,17 +196,16 @@ print(r_max)
 print('f_g: ', f_g)
 print('f_s: ', f_s)
 print('f_d: ', f_d)
-r_max = np.full(re.shape, 0.0009)
+# r_max = np.full(re.shape, 0.0009)
 ####################################################################################
 # Iteration
 ####################################################################################
 
 # initial Interface Temperature
 T_i_start = t_w
-m_ncg = m_air - m_water
 jakob = fpa.moist_air_heat_capacity(t_mean, p_standard, rH * fpa.temperature2saturation_vapour_pressure(t_in)) * \
         (t_mean - T_i_start) / fpw.enthalpy_evaporation(t_mean)
-# print('Ja: ', jakob)
+print('Ja: ', jakob)
 """correlations of parameters are added from Eimann, F., Zheng, S., Philipp, C., Omranpoor, A. H., & Gross, U.
    (2020). Dropwise condensation of humid air - Experimental investigation and modelling of the convective heat
    transfer. Int. Journal of Heat and Mass Transfer, 154
@@ -309,9 +237,9 @@ for i, item in enumerate(re):
     # print(i)
     # print('Re: ', item)
     while abs(epsilon_2[i]) > 0.05:
-        B_i[i] = np.log(mass_fraction_interface(p_standard, T_i[i]) / mass_fraction_bulk(m_ncg[i], m_water[i]))
+        B_i[i] = np.log(mf.mass_fraction_interface(p_standard, T_i[i]) / mf_bulk)
         h_d[i] = (0.347 * re[i]) / (1 + 1.75 * np.exp(-330.5 * B_i[i]))
-        sh_corr[i] = Sh[i] * corr_fog_mt(Sh[i], Nu_sen(re[i], pr[i], d_h, l), pr[i], sc[i], rH[i], T_i[i], t_mean[i]) * \
+        sh_corr[i] = Sh[i] * corr_fog_mt(Sh[i], Nu_sen(re[i], pr[i], d_h, l), pr[i], sc[i], rH[i], T_i[i], t_mean[i]) *\
             corr_suction_mt(rH[i], T_i[i], t_mean[i])
         Nu_g[i] = C[i] * (Nu_sen(re[i], pr[i], d_h, l) *
                           corr_suction_ht(Sh[i], Nu_sen(re[i], pr[i], d_h, l), pr[i], sc[i], rH[i], T_i[i], t_mean[i]) *
@@ -356,7 +284,8 @@ print('h_g: ', h_g)
 print('h_t: ', h_t)
 print('q_t: ', q_t)
 
-wdata = [[re], [pr], [nu_pt], [Nu_g], [Sh], [h_t], [h_d], [h_g], [q_t], [t_in], [t_out], [t_i], [t_dp_in], [t_dp_out], [rH], [r_max]]
+wdata = [[re], [pr], [nu_pt], [Nu_g], [Sh], [h_t], [h_d], [h_g], [q_t], [t_in], [t_out], [t_i], [t_dp_in],
+         [t_dp_out], [rH], [r_max]]
 wdata = np.reshape(wdata, (16, re.shape[0]))
 wdata = wdata.T
 __f = open(result_filename, 'w')
---
+++ read_config.py
@@ -0,0 +1,88 @@
+import configparser
+import numpy as np
+import fluid_properties_air as fpa
+import mass_fractions as mf
+
+
+def read(config_file, data_file=None, switch='config'):
+    if switch == 'config':
+        config = configparser.ConfigParser()
+        config.read(config_file)
+        cfg_numbers = config['dimless_numbers']
+        re = np.array([cfg_numbers.getfloat('reynolds')])
+        pr = np.array([cfg_numbers.getfloat('prandtl')])
+        sc = np.array([cfg_numbers.getfloat('schmidt')])
+    
+        cfg_temp = config['temperatures']
+        t_in = np.array([cfg_temp.getfloat('t_in')])
+        t_out = np.array([cfg_temp.getfloat('t_out')])
+        t_w = np.array([cfg_temp.getfloat('t_w')])
+        t_mean = np.array([cfg_temp.getfloat('t_mean')])
+        t_dp_in = np.array([cfg_temp.getfloat('t_dp_in')])
+        t_dp_out = np.array([cfg_temp.getfloat('t_dp_out')])
+        rh = np.array([cfg_temp.getfloat('rH')])
+    
+        cfg_dim = config['dimensions']
+        b = cfg_dim.getfloat('width')
+        h = cfg_dim.getfloat('height')
+        l = cfg_dim.getfloat('length')
+
+        cfg_oth = config['other']
+        p_standard = cfg_oth.getfloat('pressure')
+        theta_a = cfg_oth.getfloat('ascending_contact_angle') / (180 * np.pi)
+
+        try:
+            cfg_mf = config['mass_flow']
+            m_air = np.array([cfg_mf.getfloat('massflow_moistair')])
+            m_water = np.array([cfg_mf.getfloat('massflow_water')])
+            m_cond = np.array([cfg_mf.getfloat('massflow_condensate')])
+            mf_int = mf.mass_fraction_interface(p_standard, t_w)
+            mf_bulk = mf.mass_fraction_bulk(m_air - m_water, m_water)
+        except KeyError:
+            print('using mass fraction')
+        try:
+            cfg_mf = config['mass_fraction']
+            mf_int = np.array(cfg_mf.getfloat('mass_fraction_interface'))
+            mf_bulk = np.array(cfg_mf.getfloat('mass_fraction_bulk'))
+        except KeyError:
+            print('no mass flow or mass fractions were given.')
+            exit(0)
+            mf_int, mf_bulk = [], []
+
+        return re, pr, sc, t_in, t_out, t_w, t_mean, t_dp_in, t_dp_out, \
+            rh, mf_int, mf_bulk, b, h, l, p_standard, theta_a
+    elif switch == 'dat' and data_file is not None:
+        config = configparser.ConfigParser()
+        config.read(config_file)
+        cfg_dim = config['dimensions']
+        b = cfg_dim.getfloat('width')
+        h = cfg_dim.getfloat('height')
+        l = cfg_dim.getfloat('length')
+        p_standard = config['other'].getfloat('pressure')
+        theta_a = config['other'].getfloat('ascending_contact_angle') / (180 * np.pi)
+        re = np.loadtxt(data_file, skiprows=1, usecols=2)
+        pr = np.loadtxt(data_file, skiprows=1, usecols=4)
+    
+        t_in = np.loadtxt(data_file, skiprows=1, usecols=28)
+        t_out = np.loadtxt(data_file, skiprows=1, usecols=30)
+        t_w = np.loadtxt(data_file, skiprows=1, usecols=26)
+        t_mean = np.loadtxt(data_file, skiprows=1, usecols=32)
+        t_dp_in = np.loadtxt(data_file, skiprows=1, usecols=34)
+        t_dp_out = np.loadtxt(data_file, skiprows=1, usecols=36)
+        rh = np.loadtxt(data_file, skiprows=1, usecols=45)
+    
+        m_air = np.loadtxt(data_file, skiprows=1, usecols=50)
+        m_water = np.loadtxt(data_file, skiprows=1, usecols=40)
+        m_cond = np.loadtxt(data_file, skiprows=1, usecols=18)
+        mf_int = mf.mass_fraction_interface(p_standard, t_w)
+        mf_bulk = mf.mass_fraction_bulk(m_air - m_water, m_water)
+
+        sc = (fpa.moist_air_dynamic_viscosity(t_mean, p_standard,
+                                              rh * fpa.temperature2saturation_vapour_pressure(t_in))) / \
+             (fpa.diffusion_coefficient(t_mean) *
+              fpa.moist_air_density(p_standard, rh * fpa.temperature2saturation_vapour_pressure(t_in), t_mean))
+        return re, pr, sc, t_in, t_out, t_w, t_mean, t_dp_in, t_dp_out, \
+            rh, mf_int, mf_bulk, b, h, l, p_standard, theta_a
+    else:
+        print('switch not set or data file missing')
+        exit(0)
