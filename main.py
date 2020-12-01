a1f5a37837d2722532c0a7535cbbc1497a92c367
Chrias75 <christian_brueckner23@yahoo.de>
2020-12-01 17:02:35 +0100

    tried to adjust the validation to Eimanns matlab script, but something is still off

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ from helpers import read_config
 # Input
 ####################################################################################
 
-config_file = 'config/experiment_config.cfg'
-# config_file = 'config/model_config.cfg'
+# config_file = 'config/experiment_config.cfg'
+config_file = 'config/model_config.cfg'
 data = 'data/data_rH_2000_27_5.dat'
-result_filename = 'data/eimann_2000_31_5_h_d.dat'
+result_filename = 'data/validation_test.dat'
 re, pr, ri, sc, t_in, t_out, t_w, t_mean, t_dp_in, t_dp_out, rH, mf_int, mf_bulk, b, h, l, p_standard, \
- theta_a, theta_r, flow_direction = read_config.read(config_file, data_file=data, switch='dat')
+ theta_a, theta_r, flow_direction = read_config.read(config_file, data_file=None, switch='config')
 
 ####################################################################################
 # Logger Setup
@@ -44,22 +44,29 @@ logger.addHandler(ch)
 # Force balance parameter initialisation
 ####################################################################################
 
+# hydraulic diameter from geometry
 d_h = (4 * b * h) / (2 * b + 2 * h)
+
 # surface tension
 surf_tens = PropsSI('SURFACE_TENSION', 'T', t_mean + 273.15, 'Q', 1, 'Water')
+
 # water and air density
 rho_c = fpw.density(t_w) * 1000
 rho_b = fpa.moist_air_density(p_standard, rH * fpa.temperature2saturation_vapour_pressure(t_in), t_mean)
+
 # initial value for critical radius
 r_max = np.full(re.shape, 0.0016)
+
 # initial values for Bond number, droplet aspect ratio, minimum contact angle and drag coefficient
 bo = Bo(rho_c, r_max, surf_tens)
 beta = aspect_ratio(bo)
 theta_m_0 = minimum_contact_angle(bo, theta_a)
 c_d = np.vectorize(c_drag)(r_max, theta_a, theta_m_0, re, d_h)
+
 # bulk velocity based on reynolds number
 u = re * HAPropsSI('mu', 'T', t_mean + 273.15, 'P', p_standard, 'R', rH) / \
     (d_h * fpa.moist_air_density(p_standard, rH * fpa.temperature2saturation_vapour_pressure(t_in), t_mean))
+
 # variable initialisation for forces and iteration
 f_g, f_s, f_d = np.zeros(re.shape), np.zeros(re.shape), np.zeros(re.shape)
 epsilon_1 = np.ones(re.shape)
@@ -93,18 +100,19 @@ for i, item in enumerate(re):
         except IndexError:
             bo[i] = Bo(rho_c[i], 2 * r, np.array([surf_tens])[i])
         beta[i] = aspect_ratio(bo[i])
-        theta_m = minimum_contact_angle(bo[i], theta_a)
+        # theta_m = minimum_contact_angle(bo[i], theta_a)
+        theta_m = theta_r
         c_d[i] = c_drag(r, theta_a, theta_m, re[i], d_h)
         # a: scaling factor for the deformation of the droplet due to flow. arbitrary value based on observation
         a = 1.
         if flow_direction == 'horizontal':
-            f_g[i] = f_grav(r, rho_c[i])
+            f_g[i] = f_grav_vert(r, rho_c[i], theta_a, theta_m)
             try:
-                f_s[i] = a * f_surf_tens(r, surf_tens[i], theta_a, theta_m, beta[i])
+                f_s[i] = a * f_surf_tens_ei(r, surf_tens[i], theta_a, theta_m, beta[i])
             except TypeError:
-                f_s[i] = a * f_surf_tens(r, np.array([surf_tens])[i], theta_a, theta_m, beta[i])
+                f_s[i] = a * f_surf_tens_ei(r, np.array([surf_tens])[i], theta_a, theta_m, beta[i])
             except IndexError:
-                f_s[i] = a * f_surf_tens(r, np.array([surf_tens])[i], theta_a, theta_m, beta[i])
+                f_s[i] = a * f_surf_tens_ei(r, np.array([surf_tens])[i], theta_a, theta_m, beta[i])
             f_d[i] = f_drag(r, rho_b[i], u[i], c_d[i], theta_a, theta_m)
             epsilon_1[i] = f_g[i] ** 2 + f_d[i] ** 2 - f_s[i] ** 2
 
@@ -139,8 +147,8 @@ logger.info('f_s: {a}'.format(a=f_s))
 logger.info('f_d: {a}'.format(a=f_d))
 
 # overwriting the critical radius, if desired
-r_max = np.full(re.shape, 0.0007)
-logger.info('r_max overwritten manually; r_max: {a}'.format(a=r_max))
+# r_max = np.full(re.shape, 0.0007)
+# logger.info('r_max overwritten manually; r_max: {a}'.format(a=r_max))
 # exit(0)
 
 ####################################################################################
@@ -149,13 +157,16 @@ logger.info('r_max overwritten manually; r_max: {a}'.format(a=r_max))
 
 # wall temperature serves as initial interface temperature
 T_i_start = t_w
+
 # simplification, as jakob is using wall temperature and is assumed constant
 jakob = jakob(t_mean, T_i_start, t_in, p_standard, rH)
+
 # Eimanns correction factor for total heat transfer based on critical radius
 C = np.vectorize(correction_factor)(r_max)
 
 # Sherwood is gained analogously to Nusselt number with Schmidt instead of Prandtl number
 Sh = np.vectorize(Nu_sen)(re, sc, d_h, l)
+
 # variable initialisation for heat transfer iteration
 h_d, Nu_g, B_i, h_g, h_t, q_t, t_i, sh_corr, nu_corr = np.zeros(re.shape), np.zeros(re.shape), np.zeros(re.shape), \
                                                        np.zeros(re.shape), np.zeros(re.shape), np.zeros(re.shape), \
